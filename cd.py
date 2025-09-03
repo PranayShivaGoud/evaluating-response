c7fe970771df3c200210fe0a28e4076e4176986d
PranayShivaGoud <114612491+PranayShivaGoud@users.noreply.github.com>
2025-09-03 14:30:14 +0530

    Add files via upload

---
+++ cd.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Hello Page</title>
+    </head>
+    <body style="text-align:center; margin-top: 50px;">
+        <h1>Hello, World!</h1>
+        <p>This is served using Python + HTML</p>
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
